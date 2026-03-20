659a184a8be64ac280fea97ce282069eb05dfb30
Surakiat (Ball) Nantasaeng <ballsurakiat38@gmail.com>
2026-03-20 21:32:02 +0700

    Update Route

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// Cleanup Facebook/Social tracking parameters from URL
+const url = new URL(window.location.href);
+if (url.searchParams.has('fbclid') || url.searchParams.has('fid')) {
+  url.searchParams.delete('fbclid');
+  url.searchParams.delete('fid');
+  window.history.replaceState({}, document.title, url.pathname + url.search + url.hash);
+}
+
 createApp(App).mount('#app')
